f616ed59503cc039577f1e18ae99e88b63abd739
Titov Nikita <allureshura@mail.ru>
2023-03-12 02:44:18 +0500

    work commit

--- exercises03/src/main/scala/exercises03/MyList.scala
+++ exercises03/src/main/scala/exercises03/MyList.scala
@@ -1,5 +1,7 @@
 package exercises03
 
+import scala.annotation.tailrec
+
 sealed trait MyList[+A]
 
 final case class Cons[A](head: A, tail: MyList[A]) extends MyList[A]
@@ -7,7 +9,13 @@ final case class Cons[A](head: A, tail: MyList[A]) extends MyList[A]
 case object Nil extends MyList[Nothing]
 
 object MyList {
-  def sum(list: MyList[Int]): Int = ???
+
+  def sum(list: MyList[Int]): Int = summing(list, 0)
+  @tailrec
+  private def summing(list: MyList[Int], result: Int): Int = list match {
+    case Nil                   => result
+    case Cons[Int](head, tail) list => summing(list.tail, result + list.head)
+  }
 
   def reverse[A](list: MyList[A]): MyList[A] = ???
 }
