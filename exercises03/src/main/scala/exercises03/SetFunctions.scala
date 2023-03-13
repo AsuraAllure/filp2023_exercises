7fe0d1932eb97db907cd4aef18118bc6772b626b
Titov Nikita <allureshura@mail.ru>
2023-03-14 03:17:33 +0500

    setFunctions

--- exercises03/src/main/scala/exercises03/SetFunctions.scala
+++ exercises03/src/main/scala/exercises03/SetFunctions.scala
@@ -3,19 +3,21 @@ package exercises03
 object SetFunctions {
   type Set[A] = A => Boolean
 
-  def contains[A](s: Set[A], elem: A): Boolean = ???
+  def contains[A](s: Set[A], elem: A): Boolean = s(elem)
 
-  def singletonSet[A](elem: A): Set[A] = ???
+  def singletonSet[A](elem: A): Set[A] = (el: A) => el == elem
 
-  def union[A](s: Set[A], t: Set[A]): Set[A] = ???
+  def union[A](s: Set[A], t: Set[A]): Set[A] = (el: A) => s(el) || t(el)
 
-  def intersect[A](s: Set[A], t: Set[A]): Set[A] = ???
+  def intersect[A](s: Set[A], t: Set[A]): Set[A] = (el: A) => s(el) && t(el)
 
-  def diff[A](s: Set[A], t: Set[A]): Set[A] = ???
+  def diff[A](s: Set[A], t: Set[A]): Set[A] = (el: A) => s(el) && !t(el)
 
-  def symmetricDiff[A](s: Set[A], t: Set[A]): Set[A] = ???
+  def symmetricDiff[A](s: Set[A], t: Set[A]): Set[A] = (el: A) => t(el) && !s(el) || s(el) && !t(el)
 
-  def filter[A](s: Set[A], p: A => Boolean): Set[A] = ???
+  def filter[A](s: Set[A], p: A => Boolean): Set[A] = (el: A) => s(el) && p(el)
 
-  def cartesianProduct[A, B](as: Set[A], bs: Set[B]): Set[(A, B)] = ???
+  def cartesianProduct[A, B](as: Set[A], bs: Set[B]): Set[(A, B)] =
+    (elems: (A, B)) => as(elems._1) && bs(elems._2)
+
 }
