242fe3c1e54bbe31fd424981ab188bb84c46d30c
Titov Nikita <allureshura@mail.ru>
2023-03-14 04:35:43 +0500

    Game

--- exercises03/src/main/scala/exercises03/game/Game.scala
+++ exercises03/src/main/scala/exercises03/game/Game.scala
@@ -1,9 +1,37 @@
 package exercises03.game
 
 object Game {
-  def parseState(input: String, number: Int): State = ???
+  def parseState(input: String, number: Int): State = input.toIntOption match {
+    case None =>
+      if (input == GameController.IGiveUp) {
+        GiveUp
+      } else {
+        WrongInput
+      }
+    case inputNumber =>
+      if (inputNumber.get == number) {
+        Guessed
+      } else {
+        if (inputNumber.get < number)
+          NumberIsBigger
+        else
+          NumberIsSmaller
+      }
+  }
 
-  def action(state: State, number: Int): GameController => Unit = ???
+  def action(state: State, number: Int): GameController => Unit =
+    (controller: GameController) =>
+      state match {
+        case Guessed         => controller.guessed()
+        case GiveUp          => controller.giveUp(number)
+        case WrongInput      => controller.wrongInput()
+        case NumberIsBigger  => controller.numberIsBigger()
+        case NumberIsSmaller => controller.numberIsSmaller()
+      }
 
-  def completed(state: State): Boolean = ???
+  def completed(state: State): Boolean = state match {
+    case GiveUp  => true
+    case Guessed => true
+    case _       => false
+  }
 }
