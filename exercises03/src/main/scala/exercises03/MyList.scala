822d24b1ae5c55721c64bd0ee5aa8922806a10bc
Titov Nikita <allureshura@mail.ru>
2023-03-14 12:35:21 +0500

    Rework Function, MyList

--- exercises03/src/main/scala/exercises03/MyList.scala
+++ exercises03/src/main/scala/exercises03/MyList.scala
@@ -10,23 +10,29 @@ case object Nil extends MyList[Nothing]
 
 object MyList {
 
-  def sum(list: MyList[Int]): Int = summing(list, 0)
-  @tailrec
-  private def summing(list: MyList[Int], result: Int): Int = list match {
-    case Nil              => result
-    case Cons(head, tail) => summing(tail, result + head)
+  def sum(list: MyList[Int]): Int ={
+    @tailrec
+    def summing(list: MyList[Int], result: Int): Int = list match {
+      case Nil => result
+      case Cons(head, tail) => summing(tail, result + head)
+    }
+    summing(list, 0)
   }
 
-  def reverse[A](list: MyList[A]): MyList[A] = reverseTailRec(list, Nil)
 
-  @tailrec
-  private def reverseTailRec[A](list: MyList[A], accumulating: MyList[A]): MyList[A] = list match {
-    case Nil => Nil
-    case Cons(head, tail) =>
-      if (tail == Nil) {
-        Cons(head, accumulating)
-      } else {
-        reverseTailRec(tail, Cons(head, accumulating))
-      }
+  def reverse[A](list: MyList[A]): MyList[A] = {
+    @tailrec
+    def reverseTailRec[A](list: MyList[A], accumulating: MyList[A]): MyList[A] = list match {
+      case Nil => Nil
+      case Cons(head, tail) =>
+        if (tail == Nil) {
+          Cons(head, accumulating)
+        } else {
+          reverseTailRec(tail, Cons(head, accumulating))
+        }
+    }
+    reverseTailRec(list, Nil)
   }
+
+
 }
