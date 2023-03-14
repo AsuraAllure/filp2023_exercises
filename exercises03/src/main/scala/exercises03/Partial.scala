b390180785a0a914250df6f52a984e72f5e450a1
Titov Nikita <allureshura@mail.ru>
2023-03-14 12:31:37 +0500

    Rework Partial

--- exercises03/src/main/scala/exercises03/Partial.scala
+++ exercises03/src/main/scala/exercises03/Partial.scala
@@ -2,7 +2,7 @@ package exercises03
 
 object Partial {
   def combo[I, T](funcs: List[PartialFunction[I, T]]): I => Option[T] =
-    if (funcs.isEmpty) (i: I) => None
-    else
-      (arg: I) => funcs.reduce((arg1, arg2) => if (arg1.lift(arg).isEmpty) arg2 else arg1).lift(arg)
+    (arg: I) =>
+      funcs.foldLeft(PartialFunction.empty[I, T])((arg1, arg2) => if (arg1.lift(arg).isEmpty) arg2 else arg1).lift(arg)
+
 }
