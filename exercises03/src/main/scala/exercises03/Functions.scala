078640eb164c5fc4185d6ae47c1efb5f41949194
Titov Nikita <90641965+AsuraAllure@users.noreply.github.com>
2023-03-17 00:13:47 +0500

    Delete Functions.scala

--- exercises03/src/main/scala/exercises03/Functions.scala
+++
@@ -1,6 +0,0 @@
-package exercises03
-
-object Functions {
-  def curry[A, B, C](f: (A, B) => C): A => B => C   = (a: A) => (b: B) => f(a, b)
-  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a: A, b: B) => f(a)(b)
-}
